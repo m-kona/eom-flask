4ee42732c552f30a4feb30c04342668ec433a13d
King-Frank-Pablo <konamasibulele@gmail.com>
2021-08-06 16:02:53 +0200

    new commit

---
+++ app.py
@@ -0,0 +1,232 @@
+import hmac
+import sqlite3
+import datetime
+
+from flask import Flask, request, jsonify
+from flask_jwt import JWT, jwt_required, current_identity
+from flask_cors import CORS
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+
+
+def user_table():
+    conn = sqlite3.connect('flasko.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "first_name TEXT NOT NULL,"
+                 "last_name TEXT NOT NULL,"
+                 "email TEXT NOT NULL,"
+                 "username TEXT NOT NULL,"
+                 "password TEXT NOT NULL)")
+    print("user table created successfully")
+    conn.close()
+
+def product_table():
+    with sqlite3.connect('flasko.db') as conn:
+        conn.execute('CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                     'product_name TEXT NOT NULL,'
+                     'price TEXT NOT NULL,'
+                     'description TEXT NOT NULL)')
+        print("product table created successfuly")
+
+product_table()
+user_table()
+
+def fetch_users():
+    with sqlite3.connect('flasko.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user")
+        users = cursor.fetchall()
+
+        new_data = []
+
+        for data in users:
+            new_data.append(User(data[0], data[3], data[4]))
+    return new_data
+
+
+users = fetch_users()
+
+username_table = { u.username: u for u in users }
+userid_table = { u.id: u for u in users }
+
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and hmac.compare_digest(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+
+app = Flask(__name__)
+CORS(app)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+@app.route('/user-registration/', methods=["POST"])
+def user_registration():
+    response = {}
+
+    if request.method == "POST":
+
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        with sqlite3.connect("flasko.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO user("
+                           "first_name,"
+                           "last_name,"
+                           "email,"
+                           "username,"
+                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, email, username, password))
+            conn.commit()
+            response["message"] = "success"
+            response["status_code"] = 201
+        return response
+
+@app.route('/get-user/', methods=["GET"])
+def get_user():
+    response = {}
+    with sqlite3.connect("flasko.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user")
+
+        posts = cursor.fetchall()
+
+    response['status_code'] = 200
+    response['data'] = posts
+    return response
+
+
+@app.route('/create-products/', methods=["POST"])
+# @jwt_required()
+def create_blog():
+    response = {}
+
+    if request.method == "POST":
+        product_name = request.form['product_name']
+        price = request.form['price']
+        description = request.form['description']
+
+        with sqlite3.connect('flasko.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO product("
+                           "product_name,"
+                           "price,"
+                           "description) VALUES(?, ?, ?)", (product_name, price, description))
+            conn.commit()
+            response["status_code"] = 201
+            response['description'] = " product added succesfully"
+        return response
+
+
+@app.route('/get-blogs/', methods=["GET"])
+def get_blogs():
+    response = {}
+    with sqlite3.connect("flasko.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM product")
+
+        posts = cursor.fetchall()
+
+    response['status_code'] = 200
+    response['data'] = posts
+    return response
+
+
+
+@app.route("/delete-post/<int:post_id>")
+# @jwt_required()
+def delete_post(post_id):
+    response = {}
+    with sqlite3.connect("flasko.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM product WHERE id=" + str(post_id))
+        conn.commit()
+        response['status_code'] = 200
+        response['message'] = "product deleted successfully."
+    return response
+
+
+
+@app.route('/edit-post/<int:post_id>/', methods=["PUT"])
+@jwt_required()
+def edit_post(post_id):
+    response = {}
+
+    if request.method == "PUT":
+        with sqlite3.connect('flasko.db') as conn:
+            incoming_data = dict(request.json)
+            put_data = {}
+
+            if incoming_data.get("product_name") is not None:
+                put_data["product_name"] = incoming_data.get("product_name")
+                with sqlite3.connect('flasko.db') as conn:
+                    cursor = conn.cursor()
+                    cursor.execute("UPDATE post SET product_name =? WHERE id=?", (put_data["product_name"], post_id))
+                    conn.commit()
+                    response['message'] = "Update was successfully"
+                    response['status_code'] = 200
+            if incoming_data.get("price") is not None:
+                put_data['price'] = incoming_data.get('price')
+
+                with sqlite3.connect('flasko.db') as conn:
+                    cursor = conn.cursor()
+                    cursor.execute("UPDATE post SET price =? WHERE id=?", (put_data["price"], post_id))
+                    conn.commit()
+
+                    response["price"] = "price updated successfully"
+                    response["status_code"] = 200
+
+            if incoming_data.get("description") is not None:
+                put_data['description'] = incoming_data.get('description')
+                with sqlite3.connect('flasko.db') as conn:
+                    cursor = conn.cursor()
+                    cursor.execute("UPDATE product SET price =? WHERE id=?", (put_data["description"], post_id))
+                    conn.commit()
+
+                    response["description"] = "description updated successfully"
+                    response["status_code"] = 200
+    return response
+
+
+@app.route('/get-post/<int:post_id>/', methods=["GET"])
+def get_post(post_id):
+    response = {}
+
+    with sqlite3.connect("flasko.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM product WHERE id=" + str(post_id))
+
+        response["status_code"] = 200
+        response["description"] = "products retrieved successfully"
+        response["data"] = cursor.fetchone()
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
